e0b07d1d8fff395c73defc5f14b7b33893cb8cf5
Ray <mathius2417@gmail.com>
2023-10-18 07:20:39 -0500

    Mejorao

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-
 public class Main {
-    public static void main(String[] args){
-        Materias materia1 = new Materias();
-    }
+    public static void main(String[] args) {
 
+        // Creamos un objeto de la clase Estudiante
+        Estudiante estudiante = new Estudiante();
+
+        // Ingresamos los datos del estudiante
+        estudiante.ingresarDatos();
+
+        // Imprimimos el contenido del estudiante
+       // estudiante.imprimirDatos();
+
+        // Imprimimos las materias del estudiante
+        estudiante.imprimirMaterias();
+
+        // Imprimimos los nombres de los profesores del estudiante
+        estudiante.imprimirTodosProfesores();
     }
+}
+
